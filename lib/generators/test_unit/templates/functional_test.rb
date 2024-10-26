d06f4eb7ef33a76ac06fd3c1e9c5da3ff66a72e3
Yaro Shm <yshmarov@gmail.com>
2024-10-26 23:16:56 +0200

    rails 7 works (#1)

--- lib/generators/test_unit/templates/functional_test.rb
+++ lib/generators/test_unit/templates/functional_test.rb
@@ -1,47 +1,47 @@
 require 'test_helper'
 
-class <%= controller_class_name %>ControllerTest < ActionController::TestCase
+class <%= controller_class_name %>ControllerTest < ActionDispatch::IntegrationTest
   setup do
     @<%= nested_parent_name %> = <%= plural_nested_parent_name %>(:one)
     @<%= singular_table_name %> = <%= table_name %>(:one)
   end
 
   test "should get index" do
-    get :index, params: { <%= nested_parent_id %>: @<%= nested_parent_name %> }
+    get <%= nested_parent_name %>_<%= index_helper %>_url(@<%= nested_parent_name %>)
     assert_response :success
   end
 
   test "should get new" do
-    get :new, params: { <%= nested_parent_id %>: @<%= nested_parent_name %> }
+    get new_<%= nested_parent_name %>_<%= singular_table_name %>_url(@<%= nested_parent_name %>)
     assert_response :success
   end
 
   test "should create <%= singular_table_name %>" do
     assert_difference('<%= class_name %>.count') do
-      post :create, params: { <%= nested_parent_id %>: @<%= nested_parent_name %>, <%= singular_table_name %>: @<%= singular_table_name %>.attributes }
+      post <%= nested_parent_name %>_<%= index_helper %>_url(@<%= nested_parent_name %>), params: { <%= singular_table_name %>: @<%= singular_table_name %>.attributes }
     end
 
     assert_redirected_to <%= nested_parent_name %>_<%= singular_table_name %>_path(@<%= nested_parent_name %>, <%= class_name %>.last)
   end
 
   test "should show <%= singular_table_name %>" do
-    get :show, params: { <%= nested_parent_id %>: @<%= nested_parent_name %>, id: @<%= singular_table_name %> }
+    get <%= nested_parent_name %>_<%= singular_table_name %>_url(@<%= nested_parent_name %>, @<%= singular_table_name %>)
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, params: { <%= nested_parent_id %>: @<%= nested_parent_name %>, id: @<%= singular_table_name %> }
+    get edit_<%= nested_parent_name %>_<%= singular_table_name %>_url(@<%= nested_parent_name %>, @<%= singular_table_name %>)
     assert_response :success
   end
 
   test "should update <%= singular_table_name %>" do
-    put :update, params: { <%= nested_parent_id %>: @<%= nested_parent_name %>, id: @<%= singular_table_name %>, <%= singular_table_name %>: @<%= singular_table_name %>.attributes }
+    patch <%= nested_parent_name %>_<%= singular_table_name %>_url(@<%= nested_parent_name %>, @<%= singular_table_name %>), params: { <%= singular_table_name %>: @<%= singular_table_name %>.attributes }
     assert_redirected_to <%= nested_parent_name %>_<%= singular_table_name %>_path(@<%= nested_parent_name %>, <%= class_name %>.last)
   end
 
   test "should destroy <%= singular_table_name %>" do
     assert_difference('<%= class_name %>.count', -1) do
-      delete :destroy, params: { <%= nested_parent_id %>: @<%= nested_parent_name %>, id: @<%= singular_table_name %> }
+      delete <%= nested_parent_name %>_<%= singular_table_name %>_url(@<%= nested_parent_name %>, @<%= singular_table_name %>)
     end
 
     assert_redirected_to <%= nested_parent_name %>_<%= index_helper %>_path(@<%= nested_parent_name %>)
